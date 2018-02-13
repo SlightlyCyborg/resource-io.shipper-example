7cbaf7822b00e231a932b5ab327edd74ef367882
slightlycyborg <collinalexbell@gmail.com>
2018-02-12 20:26:36 -0600

    Made a custom Repository implementation that just uses in memory persistence

---
+++ user-service/repo.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+	pb "resource-io/user-service/auth"
+)
+
+type Repository interface {
+	GetAll() ([]*pb.User, error)
+	Get(id string) (*pb.User, error)
+	Create(user *pb.User) error
+	GetByEmail(email string) (*pb.User, error)
+}
+
+//For now, I am only implementing an in memory version of these calls. This is to help me get practice!
+
+type InMemRepo struct {
+	users []*pb.User
+}
+
+func (repo *InMemRepo) GetAll() ([]*pb.User, error) {
+	return repo.users, nil
+}
+
+func (repo *InMemRepo) Get(id string) (*pb.User, error) {
+	var selected_u *pb.User
+	selected_u = nil
+
+	for _, user := range repo.users {
+		if selected_u.Id == id {
+			selected_u = user
+		}
+	}
+
+	if selected_u == nil {
+		var err error
+		err = errors.New("Couldn't find user with that id")
+		return nil, err
+	}
+
+	return selected_u, nil
+}
+
+func (repo *InMemRepo) Create(user *pb.User) error {
+	repo.users = append(repo.users, user)
+	return nil
+}
+
+func (repo *InMemRepo) GetByEmail(email string) (*pb.User, error) {
+	var selected_u *pb.User
+	selected_u = nil
+
+	for _, user := range repo.users {
+		if user.Email == email {
+			//Does not return, this gets the last created entry with this email
+			selected_u = user
+		}
+	}
+
+	if selected_u == nil {
+		var err error
+		err = errors.New("Couldn't find user with that email")
+		return nil, err
+	}
+
+	return selected_u, nil
+}
